b9154892b3316491771f64a7eadb6d2c7fbe3e08
Jongjate Choomjairug <jongjate.ch@gmail.com>
2022-07-03 15:25:18 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+
+
 import './App.css';
 import {useState,useEffect} from "react"
 
 function App() {
 
-  const [contries,setCountries] = useState([])
+  const [countries,setCountries] = useState([])
+  const [word,setWord] = useState("")
+  const [dataFilter] = useState(["name","capital"]) 
 
   useEffect(()=>{
     fetch("https://restcountries.com/v2/all")
@@ -13,13 +17,37 @@ function App() {
     })
   },[])
 
+  const formatNumber=(num)=> {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
+
+  const searchCountries=(countries)=>{
+    return countries.filter((item)=>{
+      // eslint-disable-next-line 
+        return dataFilter.some((filter)=>{
+          if(item[filter]){
+          return item[filter].toString().toLowerCase().indexOf(word.toLowerCase())>-1
+          }
+        })
+    })
+}
+
   return (
     <div className="container">
+      <div className='search-container'>
+        <label htmlFor='search-from'>
+            <input type="text" className='search-input' placeholder='search anything here (capital,county)' 
+              value = {word}
+              onChange={(e)=>setWord(e.target.value)}
+              />
+        </label> 
+      </div>
       <ul className="row">
-        {contries.map((item,index)=>{
+      {searchCountries(countries).map((item,index)=>{
           return (
             <li key={index}> 
               <div className="card">
+                
                 <div className="card-title">
                   <img src={item.flag} alt={item.name}/>               
                 </div>
@@ -27,7 +55,7 @@ function App() {
                   <div className='card-description'>
                     <h2>{item.name}</h2>
                     <ol className='card-list'>
-                        <li>Population : <span>{item.population} ppl</span></li>
+                        <li>Population : <span>{formatNumber(item.population)} ppl</span></li>
                         <li>Region : <span>{item.region}</span></li>
                         <li>Capital : <span>{item.capital}</span></li>
                     </ol>
